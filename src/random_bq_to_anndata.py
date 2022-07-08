58acea5d4e70859b51c7ce93792209aab466e37d
Rebecca Asch <rasch@broadinstitute.org>
2022-07-08 14:40:19 -0400

    improvements

--- src/random_bq_to_anndata.py
+++ src/random_bq_to_anndata.py
@@ -8,18 +8,32 @@ def get_random_ids(project, dataset, num_cells):
     client = bigquery.Client()
     query = client.query(f"SELECT MAX(cas_cell_index) AS max_table_number FROM `{project}.{dataset}.cas_cell_info`")
     max_cell_id = int([row.max_table_number for row in list(query.result())][0])
-
     print(f"Getting {num_cells} random IDs between 0 and {max_cell_id}...")
-    return random.choices(range(0, max_cell_id, 1), k = num_cells)
+    cell_ids = list(range(0, max_cell_id))
+    random.shuffle(cell_ids)
+    del cell_ids[num_cells:]
+    return cell_ids
 
 
 def get_cell_data(project, dataset, num_cells):
     random_ids = get_random_ids(project, dataset, num_cells)
 
-    in_clause = f"WHERE cas_cell_index IN ({','.join(map(str, random_ids))})"
-    print(in_clause)
+    in_clause = f" matrix.cas_cell_index IN ({','.join(map(str, random_ids))})"
     client = bigquery.Client()
-    # query = client.query(f"SELECT ? FROM `{project}.{dataset}.cas_cell_info` " + in_clause)
+
+    # at some point, we will probably want create temp table of cell_ids and then JOIN on it
+    # instead of an IN clause
+    sql = f"SELECT original_cell_id, cell_type, original_gene_id, feature_name AS gene_feature, raw_counts AS count FROM `{project}.{dataset}.cas_cell_info` AS cell, `{project}.{dataset}.cas_gene_info` AS gene, `{project}.{dataset}.cas_raw_count_matrix` AS matrix WHERE matrix.cas_cell_index = cell.cas_cell_index AND matrix.cas_gene_index = gene.cas_gene_index AND" + in_clause
+    print(f"Getting {num_cells} random cell data...")
+    query = client.query(sql)
+    return query.result()
+
+
+def random_bq_to_anndata(project, dataset, num_cells):
+    cell_data = get_cell_data(project, dataset, num_cells)
+    adata = ad.AnnData()
+    for row in list(cell_data):
+        ...
 
 
 if __name__ == '__main__':
@@ -29,4 +43,4 @@ if __name__ == '__main__':
     parser.add_argument('--num_cells', type=int, help='Number of cells to return', required=True)
 
     args = parser.parse_args()
-    get_cell_data(args.project, args.dataset, args.num_cells)
+    random_bq_to_anndata(args.project, args.dataset, args.num_cells)
