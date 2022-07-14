bb53a708038cc3779fd799d69cdf22fb45c5d5ad
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-07-14 05:58:55 -0400

    more avro

--- src/anndata_to_bq.py
+++ src/anndata_to_bq.py
@@ -13,7 +13,29 @@ def current_milli_time():
     return round(time.time() * 1000)
 
 
-def dump_core_matrix(x, row_lookup, col_lookup, basename):
+def write_avro(generator, parsed_schema, filename, flush_batch_size, progress_batch_size):
+    start = current_milli_time()
+    with open(filename, 'a+b') as out:
+        records = []
+        counter = 0
+        for record in generator():
+            counter = counter + 1
+            records.append(record)
+
+            if counter % flush_batch_size == 0:
+                writer(out, parsed_schema, records)
+                records = []
+
+            if counter % progress_batch_size == 0:
+                end = current_milli_time()
+                print(f"    Processed {counter} rows... in {end-start} ms")
+                start = end
+
+        if len(records) > 0:
+            writer(out, parsed_schema, records)
+
+
+def dump_core_matrix(x, row_lookup, col_lookup, basename, flush_batch_size, progress_batch_size):
     schema = {
         'doc': 'A raw datum indexed by cell and feature id in the CAS BigQuery schema',
         'namespace': 'cas',
@@ -27,12 +49,7 @@ def dump_core_matrix(x, row_lookup, col_lookup, basename):
     }
     parsed_schema = parse_schema(schema)
 
-    counter = 0
-    records = []
-    start = current_milli_time()
-
-    # Write data to an Avro file
-    with open(f'{basename}_raw_counts.avro', 'a+b') as out:
+    def raw_counts_generator():
         cx = x.tocoo(copy=False)
 
         for i, j, v in zip(cx.row, cx.col, cx.data):
@@ -41,30 +58,18 @@ def dump_core_matrix(x, row_lookup, col_lookup, basename):
             
             # Todo -- how can we ensure this is safe/right?
             v_int = int(v)
-            records.append({
+            yield {
                 u'cas_cell_index': cas_cell_index.item(),
                 u'cas_feature_index': cas_feature_index.item(),
                 u'raw_counts': v_int
-            })
-            counter = counter + 1
+            }
 
-            # Write in batches for reasonable performance, one by one is extremely slow.
-            if counter % 10000 == 0:
-                writer(out, parsed_schema, records)
-                records = []
-
-            if counter % 1000000 == 0:
-                end = current_milli_time()
-                print(f"    Processed {counter} rows... in {end-start} ms")
-                start = end
-
-        # Write any leftover records.
-        if len(records) > 0:
-            writer(out, parsed_schema, records)
+    write_avro(raw_counts_generator, parsed_schema, f'{basename}_raw_counts.avro',
+               flush_batch_size, progress_batch_size)
 
 
-def dump_cell_info(adata, basename, cas_cell_index_start):
-    # dump out cell info (obs) -- cell_id is index (26 columns)
+def dump_cell_info(adata, basename, cas_cell_index_start, flush_batch_size, progress_batch_size):
+    # Dump out cell info (obs) -- cell_id is index (26 columns).
     adata.obs['original_cell_id'] = adata.obs.index
     adata.obs['cas_cell_index'] = np.arange(cas_cell_index_start, cas_cell_index_start + len(adata.obs))
 
@@ -95,28 +100,14 @@ def dump_cell_info(adata, basename, cas_cell_index_start):
                 u'cell_type': r['cell_type']
             }
 
-    def write_cells(generator, filename):
-        with open(filename, 'a+b') as out:
-            records = []
-            counter = 0
-            for record in generator():
-                counter = counter + 1
-                records.append(record)
-
-                if counter % 10000 == 0:
-                    writer(out, parsed_schema, records)
-                    records = []
-
-            if len(records) > 0:
-                writer(out, parsed_schema, records)
-
-    write_cells(cell_generator, f'{basename}_cell_info.avro')
+    write_avro(cell_generator, parsed_schema, f'{basename}_cell_info.avro',
+               flush_batch_size, progress_batch_size)
 
     return row_index_to_cas_cell_index
 
 
-def dump_feature_info(adata, basename, cas_feature_index_start):
-    # dump out feature info -- feature_id is index (12 columns)
+def dump_feature_info(adata, basename, cas_feature_index_start, flush_batch_size, progress_batch_size):
+    # Dump out feature info -- feature_id is index (12 columns).
     adata.var['original_feature_id'] = adata.var.index
 
     adata.var['cas_feature_index'] = np.arange(cas_feature_index_start, cas_feature_index_start + len(adata.var))
@@ -137,43 +128,40 @@ def dump_feature_info(adata, basename, cas_feature_index_start):
     }
 
     parsed_schema = parse_schema(schema)
-    records = []
-    counter = 0
 
-    with open(f'{basename}_feature_info.avro', 'a+b') as out:
+    def feature_generator():
         features = adata.var[['cas_feature_index', 'original_feature_id', 'feature_name']]
         for _, row in features.iterrows():
-            counter = counter + 1
-            records.append({
+            yield {
                 u'cas_feature_index': row['cas_feature_index'],
                 u'original_feature_id': row['original_feature_id'],
                 u'feature_name': row['feature_name']
-            })
+            }
 
-            if counter % 10000 == 0:
-                writer(out, parsed_schema, records)
-                records = []
-
-        if len(records) > 0:
-            writer(out, parsed_schema, records)
-
+    write_avro(feature_generator, parsed_schema, f'{basename}_feature_info.avro',
+               flush_batch_size, progress_batch_size)
     return col_index_to_cas_feature_index
 
 
-def process(input_file, cas_cell_index_start, cas_feature_index_start, basename):
+def process(input_file, cas_cell_index_start, cas_feature_index_start, basename, flush_batch_size, progress_batch_size):
     basename = "cas" if not basename else basename
+    flush_batch_size = 10000 if not flush_batch_size else flush_batch_size
+    progress_batch_size = 1000000 if not progress_batch_size else progress_batch_size
     print("Loading data...")
     adata = ad.read(input_file)
 
     print("Processing cell/observation metadata...")
-    row_index_to_cas_cell_index = dump_cell_info(adata, basename, cas_cell_index_start)
+    row_index_to_cas_cell_index = dump_cell_info(adata, basename, cas_cell_index_start,
+                                                 flush_batch_size, progress_batch_size)
     # dump out feature info -- feature_id is index (12 columns)
     print("Processing feature/gene/variable metadata...")
-    col_index_to_cas_feature_index = dump_feature_info(adata, basename, cas_feature_index_start)
+    col_index_to_cas_feature_index = dump_feature_info(adata, basename, cas_feature_index_start,
+                                                       flush_batch_size, progress_batch_size)
 
     print("Processing core data...")
     # recode the indexes to be the indexes of obs/var or should obs/var include these indices?
-    dump_core_matrix(adata.raw.X, row_index_to_cas_cell_index, col_index_to_cas_feature_index, basename)
+    dump_core_matrix(adata.raw.X, row_index_to_cas_cell_index, col_index_to_cas_feature_index, basename,
+                     flush_batch_size, progress_batch_size)
         
 # TODO: naive dump, compare
 #    rows,cols = adata.X.nonzero()
@@ -195,8 +183,12 @@ if __name__ == '__main__':
                         default=0)
     parser.add_argument('--cas_feature_index_start', type=int, help='starting number for feature index', required=False,
                         default=0)
+    parser.add_argument('--flush_batch_size', type=int, help='max size of Avro batches to flush', required=False)
+    parser.add_argument('--progress_batch_size', type=int, help='size of batches on which to report progress',
+                        required=False)
 
     # Execute the parse_args() method
     args = parser.parse_args()
 
-    process(args.input, args.cas_cell_index_start, args.cas_feature_index_start, args.basename)
+    process(args.input, args.cas_cell_index_start, args.cas_feature_index_start, args.basename,
+            args.flush_batch_size, args.progress_batch_size)
