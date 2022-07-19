1cf26e8fe836272fddd14baa97d6237a56f5d56f
gbggrant <ggrant@broadinstitute.org>
2022-07-19 16:32:15 -0400

    Changed to handle the fact that we are storing ALL features Also reworked to read the cells and features (and their metadata) separately from the matrix query.

--- src/random_bq_to_anndata.py
+++ src/random_bq_to_anndata.py
@@ -5,42 +5,115 @@ import numpy as np
 import anndata as ad
 from scipy.sparse import csr_matrix
 
-# assumes that cas_cell_info.cas_feature_index values are a contiguous list of ints
-def get_random_ids(project, dataset, client, num_cells):
+class Cell:
+    cas_cell_index = 0
+    original_cell_id = ""
+    cell_type = ""
+
+    def __init__(self, cas_cell_index, original_cell_id, cell_type):
+        self.cas_cell_index = cas_cell_index
+        self.original_cell_id = original_cell_id
+        self.cell_type = cell_type
+
+class Feature:
+    cas_feature_index = 0
+    original_feature_id = ""
+    feature_name = ""
+
+    def __init__(self, cas_feature_index, original_feature_id, feature_name):
+        self.cas_feature_index = cas_feature_index
+        self.original_feature_id = original_feature_id
+        self.feature_name = feature_name
+
+# assumes that cas_cell_info.cas_cell_index values are a contiguous list of ints
+def get_random_cell_ids(project, dataset, client, num_cells):
     query = client.query(f"SELECT MIN(cas_cell_index) AS min_cas_cell_index, MAX(cas_cell_index) AS max_cas_cell_index FROM `{project}.{dataset}.cas_cell_info`")
     row = list(query.result())[0]
     min_cas_cell_index, max_cas_cell_index = row.min_cas_cell_index, row.max_cas_cell_index
     print(f"Getting {num_cells} random IDs between {min_cas_cell_index} and {max_cas_cell_index}...")
     cell_ids = list(range(min_cas_cell_index, max_cas_cell_index + 1))
-    random.shuffle(cell_ids)
+    # random.shuffle(cell_ids)
     del cell_ids[num_cells:]
     print(f"Random IDs: {cell_ids}")
     return cell_ids
 
+# Retrieve all of the features from the database
+def get_features(project, dataset, client):
+    sql = f"SELECT cas_feature_index, original_feature_id, feature_name FROM `{project}.{dataset}.cas_feature_info` ORDER BY cas_feature_index"
+    query = client.query(sql)
+    ordered_feature_indices = []
+    feature_index_to_feature = {}
+    for row in query:
+        index = row["cas_feature_index"]
+        ordered_feature_indices.append(index)
+        feature_index_to_feature[index] = Feature(row["cas_feature_index"], row["original_feature_id"], row["feature_name"])
+    return ordered_feature_indices, feature_index_to_feature
 
-def get_cell_data(project, dataset, client, num_cells):
-    random_ids = get_random_ids(project, dataset, client, num_cells)
-    in_clause = f" matrix.cas_cell_index IN ({','.join(map(str, random_ids))})"
+# Retrieve metadata for the selected (random) cells from the database
+def get_cells(project, dataset, client, random_cell_ids):
+    in_clause = f" cas_cell_index IN ({','.join(map(str, random_cell_ids))})"
+    sql = f"SELECT cas_cell_index, original_cell_id, cell_type FROM `{project}.{dataset}.cas_cell_info` WHERE " + in_clause + " ORDER BY cas_cell_index"
+    query = client.query(sql)
+    ordered_cell_indices = []
+    cell_index_to_cell = {}
+    for row in query:
+        index = row["cas_cell_index"]
+        ordered_cell_indices.append(index)
+        cell_index_to_cell[index] = Cell(row["cas_cell_index"], row["original_cell_id"], row["cell_type"])
+    return ordered_cell_indices, cell_index_to_cell
+
+def get_matrix_data(project, dataset, client, random_cell_ids):
+    in_clause = f" matrix.cas_cell_index IN ({','.join(map(str, random_cell_ids))})"
 
     # at some point, we will probably want create temp table of cell_ids and then JOIN on it
     # instead of an IN clause
-    sql = f"SELECT matrix.cas_cell_index, original_cell_id, cell_type, matrix.cas_feature_index, original_feature_id, feature_name, raw_counts AS count FROM `{project}.{dataset}.cas_cell_info` AS cell, `{project}.{dataset}.cas_feature_info` AS feature, `{project}.{dataset}.cas_raw_count_matrix` AS matrix WHERE matrix.cas_cell_index = cell.cas_cell_index AND matrix.cas_feature_index = feature.cas_feature_index AND" + in_clause + " ORDER BY matrix.cas_cell_index, matrix.cas_feature_index"
-    print(f"Getting {num_cells} random cells' data from {project}.{dataset}...")
+    # NOTE - really don't need to join to cas_cell_index and cas_feature_index here anymore.
+    sql = f"SELECT matrix.cas_cell_index, original_cell_id, matrix.cas_feature_index, original_feature_id, raw_counts AS count FROM `{project}.{dataset}.cas_cell_info` AS cell, `{project}.{dataset}.cas_feature_info` AS feature, `{project}.{dataset}.cas_raw_count_matrix` AS matrix WHERE matrix.cas_cell_index = cell.cas_cell_index AND matrix.cas_feature_index = feature.cas_feature_index AND" + in_clause + " ORDER BY matrix.cas_cell_index, matrix.cas_feature_index"
     query = client.query(sql)
     return query.result()
 
 
 def random_bq_to_anndata(project, dataset, num_cells, output_file_prefix):
     client = bigquery.Client(project=project)
-    # Note that this method requires that the result set returned by get_cell_data be sorted by cas_cell_index (or, could also be sorted by original_cell_id)
-    cell_data = get_cell_data(project, dataset, client, num_cells)
 
-    (index_ptr, indices, data, cell_names, cell_types, feature_ids, feature_names) = generate_sparse_matrix(cell_data)
+    print(f"Getting {num_cells} random cells' data from {project}.{dataset}...")
+    random_cell_ids = get_random_cell_ids(project, dataset, client, num_cells)
+
+    # Read the cell information and store metadata for later
+    ordered_cell_indices, cell_index_to_cell = get_cells(project, dataset, client, random_cell_ids)
+
+    original_cell_ids = []
+    cell_types = []
+    for cell_index in ordered_cell_indices:
+        original_cell_ids.append(cell_index_to_cell[cell_index].original_cell_id)
+        cell_types.append(cell_index_to_cell[cell_index].cell_type)
+
+    # Read the feature information and store metadata for later.
+    ordered_feature_indices, feature_index_to_feature = get_features(project, dataset, client)
+
+    feature_ids = []
+    feature_names = []
+    last_feature_index = None
+    for feature_index in ordered_feature_indices:
+        if (last_feature_index is None):
+            last_feature_index = feature_index - 1
+        # # We expect all entries in cas_feature_info to be stored in continuous fashion. (1, 2, 3, ...)
+        if (feature_index != last_feature_index + 1):
+            raise Exception("ERROR: Non-continuous values for `cas_feature_index` in table `cas_feature_info`")
+
+        feature_ids.append(feature_index_to_feature[feature_index].original_feature_id)
+        feature_names.append(feature_index_to_feature[feature_index].feature_name)
+        last_feature_index = feature_index
+
+    # Note that this method requires that the result set returned by get_cell_data be sorted by cas_cell_index (or, could also be sorted by original_cell_id)
+    cell_data = get_matrix_data(project, dataset, client, random_cell_ids)
+
+    (index_ptr, indices, data) = generate_sparse_matrix(cell_data, ordered_feature_indices[0])
 
     # Create the matrix from the sparse data representation generated above.
     counts = csr_matrix((data, indices, index_ptr), dtype=np.float32)
     adata = ad.AnnData(counts)
-    adata.obs_names = cell_names
+    adata.obs_names = original_cell_ids
     adata.obs["cell_type"] = cell_types
     adata.var_names = feature_ids
     adata.var["feature_name"] = feature_names
@@ -50,21 +123,11 @@ def random_bq_to_anndata(project, dataset, num_cells, output_file_prefix):
     adata.raw = adata
     adata.write(f'{output_file_prefix}.h5ad', compression="gzip")
 
-def generate_sparse_matrix(cell_data):
-    column_count = 0
-    feature_to_column = {}
-    last_cell_type = None
-
+def generate_sparse_matrix(cell_data, minimum_feature_index):
     # For representation of the sparse matrix
     index_ptr = [0]
     indices = []
     data = []
-
-    # For storage of the metadata
-    cell_names = []
-    cell_types = []
-    feature_ids = []
-    feature_names = []
 
     # Builds the sparse matrix for storing the original_cell_ids (as obs) and features (as var).
     # From: https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html
@@ -82,47 +145,26 @@ def generate_sparse_matrix(cell_data):
 
     last_original_cell_id = None
     for row in cell_data:
-        # print("cas_cell_index={}, original_cell_id={}, cell_type={}, cas_feature_index={}, original_feature_id={}, feature_name={}, count={}".format(row["cas_cell_index"], row["original_cell_id"], row["cell_type"], row["cas_feature_index"], row["original_feature_id"], row["feature_name"], row["count"]))
-        # print(f"{row}")
         original_cell_id = row["original_cell_id"]
-        cell_type = row["cell_type"]
-        original_feature_id = row["original_feature_id"]
+        col_num = row["cas_feature_index"] - minimum_feature_index
         count = row["count"]
 
         if original_cell_id != last_original_cell_id:
             if last_original_cell_id is not None:
                 # We have just started reading data for a new 'original_cell_id'.
                 index_ptr.append(len(indices))
-                cell_names.append(last_original_cell_id)
-                cell_types.append(last_cell_type)
-                print(f"finishing record for original_cell_id: {last_original_cell_id}")
-                print(f"index_ptr: {index_ptr}")
+                # print(f"finishing record for original_cell_id: {last_original_cell_id}")
+                # print(f"index_ptr: {index_ptr}")
             last_original_cell_id = original_cell_id
-            last_cell_type = cell_type
-
-        if original_feature_id not in feature_to_column:
-            # We have not seen this 'original_feature_id' before.
-            feature_to_column[original_feature_id] = column_count
-            feature_ids.append(original_feature_id)
-            feature_names.append(row["feature_name"])
-            column_count += 1
-        col_num = feature_to_column[original_feature_id]
 
         indices.append(col_num)
         data.append(count)
 
-    #TODO - catch the edge case if the last row is different from last-1
-    # And add a test - especially for last-1
-    # Deal with the last row.
     index_ptr.append(len(indices))
-    cell_names.append(last_original_cell_id)
-    cell_types.append(last_cell_type)
-    print(f"finishing record for original_cell_id: {last_original_cell_id}")
-    print(f"index_ptr: {index_ptr}")
-    # print(f"indices:   {indices}")
-    # print(f"data:      {data}")
+    # print(f"finishing record for original_cell_id: {last_original_cell_id}")
+    # print(f"index_ptr: {index_ptr}")
 
-    return index_ptr, indices, data, cell_names, cell_types, feature_ids, feature_names
+    return index_ptr, indices, data
 
 
 if __name__ == '__main__':
--- src/test_random_bq_to_anndata.py
+++ src/test_random_bq_to_anndata.py
@@ -20,33 +20,33 @@ class TestRandomBqToAnndata(unittest.TestCase):
 
         self.run_test_on_matrix(cell_ids, feature_ids, matrix, expected_index_ptr, expected_indices, expected_data)
 
-    # def test_really_sparse_matrix(self):
-    #
-    #     #       ENS1 ENS2 ENS3 ENS4
-    #     # AAA   n/a  n/a  n/a  n/a
-    #     # AAC   n/a  n/a  n/a  n/a
-    #     # AAG   n/a  n/a  88   n/a
-    #     # AAT   n/a  n/a  n/a  n/a
-    #     #
-    #     # we represent it as:
-    #     # index_ptr = [0, 1]
-    #     # indices   = [0]
-    #     # data      = [88]
-    #
-    #     cell_ids = ["AAA", "AAC", "AAG", "AAT"]
-    #     feature_ids = ["ENS1", "ENS2", "ENS3", "ENS4"]
-    #
-    #     matrix = numpy.full([len(cell_ids), len(feature_ids)], None)
-    #     matrix[2][2] = 88
-    #
-    #     expected_index_ptr = [0, 1]
-    #     expected_indices   = [0]
-    #     expected_data      = [88]
-    #
-    #     self.run_test_on_matrix(cell_ids, feature_ids, matrix, expected_index_ptr, expected_indices, expected_data)
+    def test_really_sparse_matrix(self):
+
+        #       ENS1 ENS2 ENS3 ENS4
+        # AAA   n/a  n/a  n/a  n/a
+        # AAC   n/a  n/a  n/a  n/a
+        # AAG   n/a  n/a  88   n/a
+        # AAT   n/a  n/a  n/a  n/a
+        #
+        # we represent it as:
+        # index_ptr = [0, 1]
+        # indices   = [2]
+        # data      = [88]
+
+        cell_ids = ["AAA", "AAC", "AAG", "AAT"]
+        feature_ids = ["ENS1", "ENS2", "ENS3", "ENS4"]
+
+        matrix = numpy.full([len(cell_ids), len(feature_ids)], None)
+        matrix[2][2] = 88
+
+        expected_index_ptr = [0, 1]
+        expected_indices   = [2]
+        expected_data      = [88]
+
+        self.run_test_on_matrix(cell_ids, feature_ids, matrix, expected_index_ptr, expected_indices, expected_data)
 
 
-    def test_generate_sparse_matrix(self):
+    def test_sparse_matrix(self):
 
         #       ENS1 ENS2 ENS3 ENS4
         # AAA   1    7     2   n/a
@@ -86,34 +86,22 @@ class TestRandomBqToAnndata(unittest.TestCase):
     def run_test_on_matrix(self, cell_ids, feature_ids, matrix, expected_index_ptr, expected_indices, expected_data):
         cas_rows = []
 
-        expected_cell_types = []
-        expected_feature_names = []
         for i in range (0, len(cell_ids)):
             cell_id = cell_ids[i]
-            cell_type = "CT_" + cell_id
-            expected_cell_types.append(cell_type)
             for j in range (0, len(feature_ids)):
-                feature_name = "FT_" + feature_ids[j]
-                if (len(expected_feature_names) != len(feature_ids)):
-                    expected_feature_names.append(feature_name)
                 if (matrix[i][j] is not None):
                     cas_row = {}
                     cas_row["original_cell_id"] = cell_id
-                    cas_row["cell_type"] = cell_type
-                    cas_row["original_feature_id"] = feature_ids[j]
-                    cas_row["feature_name"] = feature_name
+                    cas_row["cas_feature_index"] = j
                     cas_row["count"] = matrix[i][j]
                     cas_rows.append(cas_row)
 
-        (index_ptr, indices, data, cell_names, cell_types, feature_ids, feature_names) = generate_sparse_matrix(cas_rows)
+        (index_ptr, indices, data) = generate_sparse_matrix(cas_rows, 0)
 
         self.assertEqual(index_ptr, expected_index_ptr)
         self.assertEqual(indices, expected_indices)
         self.assertEqual(data, expected_data)
-        self.assertEqual(cell_names, cell_ids)
-        self.assertEqual(cell_types, expected_cell_types)
-        self.assertEqual(feature_ids, feature_ids)
-        self.assertEqual(feature_names, expected_feature_names)
+
 
 def main():
     unittest.main()
