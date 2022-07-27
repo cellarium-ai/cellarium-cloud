e7670ad89c38fe48f2fc11a561d405f83fd867a7
Miguel Covarrubias <mcovarr@broadinstitute.org>
2022-07-27 18:04:14 -0400

    wip

--- src/random_bq_to_anndata.py
+++ src/random_bq_to_anndata.py
@@ -1,5 +1,6 @@
 import anndata as ad
 import argparse
+from collections import defaultdict
 from google.cloud import bigquery
 import json
 import numpy as np
@@ -8,12 +9,12 @@ from scipy.sparse import coo_matrix
 
 
 class Cell:
-    def __init__(self, cas_cell_index, original_cell_id, cell_type, obs_metadata, ingest_id):
+    def __init__(self, cas_cell_index, original_cell_id, cell_type, obs_metadata, cas_ingest_id):
         self.cas_cell_index = cas_cell_index
         self.original_cell_id = original_cell_id
         self.cell_type = cell_type
         self.obs_metadata = obs_metadata
-        self.ingest_id = ingest_id
+        self.cas_ingest_id = cas_ingest_id
 
 
 class Feature:
@@ -24,21 +25,33 @@ class Feature:
         self.var_metadata = var_metadata
 
 
-# Returns an array of <num_cells> cas_cell_index values pulled at random from the cas_cell_info table
-def get_random_cell_ids(project, dataset, client, num_cells):
-    query = client.query("SELECT cas_cell_index, rand() AS rand_val FROM " +
-                         f"`{project}.{dataset}.cas_cell_info` ORDER BY rand_val LIMIT {num_cells}")
-    cas_cell_indices = []
-    for row in query:
-        cas_cell_indices.append(row["cas_cell_index"])
-    print(f"Random IDs: {cas_cell_indices}")
-    return cas_cell_indices
+# Returns an array of <num_cells> cells pulled at random from the cas_cell_info table.
+def get_random_cells(project, dataset, client, num_cells):
+    query = f"""
+        SELECT cas_cell_index, original_cell_id, cell_type, obs_metadata, cas_ingest_id, rand() AS rand_val FROM
+        `{project}.{dataset}.cas_cell_info` ORDER BY rand_val LIMIT {num_cells}
+    """
+
+    result = client.query(query)
+    cells = [
+        Cell(cas_cell_index=row["cas_cell_index"],
+             original_cell_id=row["original_cell_id"],
+             cell_type=row["cell_type"],
+             obs_metadata=row["obs_metadata"],
+             cas_ingest_id=row["cas_ingest_id"])
+        for row in result
+    ]
+    print(f"Random IDs: {[c.cas_cell_index for c in cells]}")
+    return cells
 
 
 # Retrieve a list of all feature objects, ordered by cas_feature_index
-def get_features(project, dataset, client):
-    sql = "SELECT cas_feature_index, original_feature_id, feature_name, var_metadata FROM " + \
-          f"`{project}.{dataset}.cas_feature_info` ORDER BY cas_feature_index"
+def get_features(project, dataset, client, ingest_id):
+    sql = f"""
+    SELECT cas_feature_index, original_feature_id, feature_name, var_metadata FROM 
+    `{project}.{dataset}.cas_feature_info` WHERE cas_ingest_id = "{ingest_id}" ORDER BY cas_feature_index
+    """
+
     query = client.query(sql)
     features = []
     for row in query:
@@ -47,23 +60,6 @@ def get_features(project, dataset, client):
                                 row["feature_name"],
                                 row["var_metadata"]))
     return features
-
-
-# Given a list of cell_ids (= cas_cell_index) this method will return a list of cell objects for them, ordered by
-# cas_cell_index
-def get_cells(project, dataset, client, cell_ids):
-    in_clause = f" cas_cell_index IN ({','.join(map(str, cell_ids))})"
-    sql = "SELECT cas_cell_index, original_cell_id, cell_type, obs_metadata, cas_ingest_id FROM " + \
-          f"`{project}.{dataset}.cas_cell_info` WHERE {in_clause} ORDER BY cas_cell_index"
-    query = client.query(sql)
-    cells = []
-    for row in query:
-        cells.append(Cell(row["cas_cell_index"],
-                          row["original_cell_id"],
-                          row["cell_type"],
-                          row["obs_metadata"],
-                          row["cas_ingest_id"]))
-    return cells
 
 
 def get_ingest_info(project, dataset, client, ingest_ids):
@@ -77,8 +73,9 @@ def get_ingest_info(project, dataset, client, ingest_ids):
     return info
 
 
-def get_matrix_data(project, dataset, client, random_cell_ids):
-    in_clause = f" cas_cell_index IN ({','.join(map(str, random_cell_ids))})"
+def get_matrix_data(project, dataset, client, cells):
+    str_cell_ids = map(str, [c.cas_cell_index for c in cells])
+    in_clause = f" cas_cell_index IN ({','.join(str_cell_ids)})"
 
     # If the number of cells to be selected becomes large, we may want to create a temp table of the cell_ids and then
     # JOIN on it instead of using an IN clause.
@@ -112,47 +109,55 @@ def random_bq_to_anndata(project, dataset, num_cells, output_file_prefix):
     client = bigquery.Client(project=project)
 
     print(f"Getting {num_cells} random cells' data from {project}.{dataset}...")
-    random_cell_ids = get_random_cell_ids(project, dataset, client, num_cells)
+    random_cells = get_random_cells(project, dataset, client, num_cells)
 
-    # Read the cell information and store for later.
-    cells = get_cells(project, dataset, client, random_cell_ids)
-    original_cell_ids = []
-    cas_cell_index_to_row_num = {}
-    for row_num, cell in enumerate(cells):
-        original_cell_ids.append(cell.original_cell_id)
-        cas_cell_index_to_row_num[cell.cas_cell_index] = row_num
+    cells_by_ingest_id = defaultdict(list)
+    for cell in random_cells:
+        cells_by_ingest_id[cell.cas_ingest_id].append(cell)
 
-    # Read the feature information and store for later.
-    features = get_features(project, dataset, client)
+    for ingest_id, cells in cells_by_ingest_id.items():
+        id_only = ingest_id.split('-')[-1]
+        output_file_name = f'{output_file_prefix}-{id_only}.h5ad'
+        print(f"Writing {len(cells)} cells' data to '{output_file_name}'...")
 
-    feature_ids = []
-    cas_feature_index_to_col_num = {}
-    for col_num, feature in enumerate(features):
-        feature_ids.append(feature.original_feature_id)
-        cas_feature_index_to_col_num[feature.cas_feature_index] = col_num
+        original_cell_ids = []
+        cas_cell_index_to_row_num = {}
+        for row_num, cell in enumerate(cells):
+            original_cell_ids.append(cell.original_cell_id)
+            cas_cell_index_to_row_num[cell.cas_cell_index] = row_num
 
-    # Note that this method requires that the result set returned by get_cell_data be sorted by cas_cell_index
-    matrix_data = get_matrix_data(project, dataset, client, random_cell_ids)
+        # Read the feature information and store for later.
+        features = get_features(project, dataset, client, ingest_id)
 
-    rows, columns, data = convert_matrix_data_to_coo_matrix_input_format(matrix_data, cas_cell_index_to_row_num,
-                                                                         cas_feature_index_to_col_num)
-    # Create the matrix from the sparse data representation generated above.
-    counts = coo_matrix((data, (rows, columns)), shape=(len(cells), len(features)), dtype=np.float32)
+        feature_ids = []
+        cas_feature_index_to_col_num = {}
+        for col_num, feature in enumerate(features):
+            feature_ids.append(feature.original_feature_id)
+            cas_feature_index_to_col_num[feature.cas_feature_index] = col_num
 
-    # Convert the COO matrix to CSR for loading into AnnData
-    adata = ad.AnnData(counts.tocsr(copy=False))
-    adata.obs.index = original_cell_ids
-    adata.var.index = feature_ids
-    assign_obs_var_metadata(adata.obs, [c.obs_metadata for c in cells])
-    assign_obs_var_metadata(adata.var, [f.var_metadata for f in features])
-    # TODO make this work with random subsets that span multiple ingested AnnData files
-    ingest_metadata = get_ingest_info(project, dataset, client, [cells[0].ingest_id])
-    assign_uns_metadata(adata, ingest_metadata[cells[0].ingest_id])
+        # Note that this method requires that the result set returned by get_random_cells be sorted by cas_cell_index
+        matrix_data = get_matrix_data(project, dataset, client, cells)
 
-    # See https://anndata.readthedocs.io/en/latest/generated/anndata.AnnData.raw.html?highlight=raw#anndata.AnnData.raw
-    # for why we set 'raw' thusly: "The raw attribute is initialized with the current content of an object by setting:"
-    adata.raw = adata
-    adata.write(Path(f'{output_file_prefix}.h5ad'), compression="gzip")
+        rows, columns, data = convert_matrix_data_to_coo_matrix_input_format(matrix_data, cas_cell_index_to_row_num,
+                                                                             cas_feature_index_to_col_num)
+        # Create the matrix from the sparse data representation generated above.
+        counts = coo_matrix((data, (rows, columns)), shape=(len(cells), len(features)), dtype=np.float32)
+
+        # Convert the COO matrix to CSR for loading into AnnData
+        adata = ad.AnnData(counts.tocsr(copy=False))
+        adata.obs.index = original_cell_ids
+        adata.var.index = feature_ids
+        assign_obs_var_metadata(adata.obs, [c.obs_metadata for c in cells])
+        assign_obs_var_metadata(adata.var, [f.var_metadata for f in features])
+
+        ingest_metadata = get_ingest_info(project, dataset, client, [cells[0].cas_ingest_id])
+        assign_uns_metadata(adata, ingest_metadata[cells[0].cas_ingest_id])
+
+        # See https://anndata.readthedocs.io/en/latest/generated/anndata.AnnData.raw.html?highlight=raw#anndata.AnnData.raw
+        # for why we set 'raw' thusly: "The raw attribute is initialized with the current content of an object by setting:"
+        adata.raw = adata
+        adata.write(Path(output_file_name), compression="gzip")
+    print("Done.")
 
 
 def convert_matrix_data_to_coo_matrix_input_format(cell_data, cas_cell_index_to_row_num, cas_feature_index_to_col_num):
