434011726160a84535a452d12acbce32f6d04656
Kristian Cibulskis <kcibul@broadinstitute.org>
2022-11-09 15:11:27 +0000

    v1 API

---
+++ src/casp/api/README.md
@@ -0,0 +1,48 @@
+## Running Locally
+
+Note: VM Must be on the network as Vertex Matching Engine (see docs/USING_VERTEX_MATCHING_ENGINE.md)
+
+```
+pip install -r requirements.txt
+python src/casp/api/server.py
+```
+
+## Building Docker Image
+
+```
+IMAGE_NAME=us-docker.pkg.dev/dsp-cell-annotation-service/cas/cas-api:0.1
+docker build -t $IMAGE_NAME -f Dockerfile.api .
+docker push $IMAGE_NAME
+```
+## Set up VPC 
+In order for Cloud Run services to access the ai-matching network, which is necessary to access the Vertex AI Matching Engine, Serverless VPC access must be enabled.  This comes with a small cost to run the e2-micro VMs that do the network bridging.
+
+```
+gcloud compute networks vpc-access connectors create cas-ai-matching \
+--project dsp-cell-annotation-service \
+--region=us-central1 \
+--network=ai-matching \
+--range=10.8.0.0/28 \
+--min-instances=2 \
+--max-instances=10 \
+--machine-type=e2-micro
+```
+
+## Deploying Docker Image via Cloud Run
+```
+IMAGE_NAME=us-docker.pkg.dev/dsp-cell-annotation-service/cas/cas-api:0.1
+PROJECT_ID=dsp-cell-annotation-service
+
+gcloud run deploy cas-api \
+--project $PROJECT_ID \
+--image $IMAGE_NAME \
+--region us-central1 \
+--platform managed \
+--port 8000 \
+--allow-unauthenticated \
+--vpc-connector cas-ai-matching \
+--command python --args "casp/api/server.py"
+
+
+BASE_URL="https://cas-api-vi7nxpvk7a-uc.a.run.app:8000"
+curl -X POST -H "Accept: application/json" -F "json=\"gimme-som-data\";type=application/json" -F "myfile=@local_1000.h5ad" "$BASE_URL/annotate" -o results.json
--- src/casp/api/server.py
+++ src/casp/api/server.py
@@ -1,11 +1,14 @@
 import typing as t
+import time
 
+import uuid
 import pandas as pd
 import requests
 import uvicorn
 from fastapi import FastAPI, Form, UploadFile
 from google.cloud import aiplatform, bigquery
 from pydantic import BaseSettings
+from datetime import datetime, timedelta
 
 
 # TODO --refactor packages, move into commons settings class, leverage .env file
@@ -17,13 +20,17 @@ class Settings(BaseSettings):
     knn_search_deployed_index_id: str = "deployed_4m_casp_index_v1"
     knn_search_num_matches: int = 100
     bq_cell_info_table_fqn: str = "dsp-cell-annotation-service.cas_4m_dataset.cas_cell_info"
+    bq_temp_table_dataset: str = "dsp-cell-annotation-service.cas_4m_dataset" 
     items_per_user: int = 50
 
 
 settings = Settings()
 app = FastAPI()
-
-
+    
+def __log(s):
+    dt_string = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    print(f"{dt_string} - {s}")
+    
 def __get_embeddings(myfile):
     r = requests.post(settings.model_server_url, files={"file": myfile})
 
@@ -48,57 +55,71 @@ def __get_appx_knn_matches(embeddings):
     return response
 
 
-# TODO: instead upload query_id, match_cas_cell_id into a temporary table
-# and then join to that rather than batch these queries.
 def __get_cell_type_distribution(query_ids, knn_response):
     bq_client = bigquery.Client()
-    query_pieces = []
 
+    # create temporary table
+    my_uuid = str(uuid.uuid4())[:8]
+    temp_table_fqn = f"{settings.bq_temp_table_dataset}.api_request_{my_uuid}"
+
+    __log(f"Creating Temporary Table {temp_table_fqn}")
+    schema = [
+        bigquery.SchemaField("query_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("match_cas_cell_index", "INT64", mode="REQUIRED"),
+        bigquery.SchemaField("match_score", "FLOAT", mode="REQUIRED"),
+    ]
+
+    table = bigquery.Table(temp_table_fqn, schema=schema)
+    table.expires = datetime.now() + timedelta(minutes=15)
+    bq_client.create_table(table)  # Make an API request.
+
+    __log(f"Loading Data Into {temp_table_fqn}")
+    rows_to_insert = []
     for i in range(0, len(knn_response)):
         query_id = query_ids[i]
-        result = knn_response[i]
+        for match in knn_response[i]:
+            rows_to_insert.append(tuple([str(query_id), int(match.id), float(match.distance)]))
 
-        match_ids = [match.id for match in result]
+    df = pd.DataFrame(rows_to_insert, columns =['query_id', 'match_cas_cell_index', 'match_score'])
 
-        query_pieces.append(
-            f"""
-            SELECT '{query_id}' as query_id, cell_type, count(*) cell_count
-            FROM {settings.bq_cell_info_table_fqn}
-            WHERE cas_cell_index IN ({",".join(match_ids)})
-            GROUP BY 1,2
-         """
-        )
+    job_config = bigquery.LoadJobConfig(schema=schema)
+    job = bq_client.load_table_from_dataframe(df, temp_table_fqn, job_config=job_config)  
+    job.result()  # Wait for the job to complete.
+
+    __log(f"Querying Match Cell Metadata")
+    query =    f"""
+                SELECT t.query_id, ci.cell_type, avg(t.match_score), count(*) cell_count
+                FROM `{temp_table_fqn}` t
+                JOIN `{settings.bq_cell_info_table_fqn}` ci ON t.match_cas_cell_index = ci.cas_cell_index
+                GROUP BY 1,2
+                ORDER BY 1, 3 DESC
+                """
+
+
+    query_job = bq_client.query(query)
 
     results = {}
-    batch_size = 100
-    for i in range(0, len(query_pieces), batch_size):
-        print(f"Retrieving chunk {int(i/batch_size)} from BigQuery")
-        chunk = query_pieces[i : i + batch_size]
-        final_query = " UNION ALL ".join(chunk) + " ORDER BY 1, 3 DESC"
-        query_job = bq_client.query(final_query)
+    for row in query_job:
+        query_id = row["query_id"]
+        if query_id not in results:
+            results[query_id] = {}
 
-        for row in query_job:
-            query_id = row["query_id"]
-            if query_id not in results:
-                results[query_id] = {}
-
-            results[query_id][row["cell_type"]] = row["cell_count"]
+        results[query_id][row["cell_type"]] = row["cell_count"]
 
     return results
 
 
 def __annotate(file):
-    print("TODO: Validation and Preprocessing")
-    # TODO get allow-list of ensembl ids and subset
-
-    print("Calculating Embeddings")
+    __log("Calculating Embeddings")
     (query_ids, embeddings) = __get_embeddings(file)
 
-    print("Performing kNN lookup")
+    __log("Performing kNN lookup")
     knn_response = __get_appx_knn_matches(embeddings)
 
-    print("Getting Cell Type Distributions")
+    __log("Getting Cell Type Distributions")
     d = __get_cell_type_distribution(query_ids, knn_response)
+
+    __log("Finished")
     return d
 
 
