24163693f14780ae53fd66312bcdb8ac98bb02db
Fedor Grab <fvgrab@gmail.com>
2022-11-21 15:42:59 -0500

    Chore: Add 8 workers to inference and api servers

--- src/casp/api/server.py
+++ src/casp/api/server.py
@@ -166,4 +166,4 @@ async def annotate(myfile: UploadFile):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host=settings.server_host, port=settings.server_port)
+    uvicorn.run("server:app", host=settings.server_host, port=settings.server_port, workers=8)
--- src/casp/ml/services/inference/pca/server.py
+++ src/casp/ml/services/inference/pca/server.py
@@ -31,4 +31,4 @@ async def predict(file: bytes = File()) -> t.Dict:
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, workers=8)
