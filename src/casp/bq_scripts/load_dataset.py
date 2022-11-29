aea71a15af12a2c7d01ab84c7530ddf3600e832b
Kristian Cibulskis <kcibul@broadinstitute.org>
2022-11-29 21:26:43 +0000

    WIP

--- src/casp/bq_scripts/load_dataset.py
+++ src/casp/bq_scripts/load_dataset.py
@@ -7,6 +7,7 @@
 """
 
 import argparse
+import glob
 import os
 import re
 
@@ -148,8 +149,9 @@ def process(project, dataset, avro_prefix, gcs_path_prefix):
 
     input_file_types = ["ingest_info", "cell_info", "feature_info", "raw_counts"]
     input_filenames = [f"{avro_prefix}_{file_type}.avro" for file_type in input_file_types]
-    confirm_input_files_exist(input_filenames)
+##    confirm_input_files_exist(input_filenames)
     ingest_filename, cell_filename, feature_filename, raw_counts_filename = input_filenames
+    raw_counts_pattern = f"{avro_prefix}_raw_counts.*.csv"
 
     # Grab the `cas_ingest_id` from the ingest file.
     with open(ingest_filename, "rb") as file:
@@ -170,21 +172,24 @@ def process(project, dataset, avro_prefix, gcs_path_prefix):
     staged_files = []
     gcs_path_prefix = gcs_path_prefix.rstrip("/")
     pairs = [
-        ("ingest_info", ingest_filename),
-        ("cell_info", cell_filename),
-        ("feature_info", feature_filename),
-        ("raw_count_matrix", raw_counts_filename),
+       ("ingest_info", ingest_filename, bigquery.SourceFormat.AVRO),
+       ("cell_info", cell_filename, bigquery.SourceFormat.AVRO),
+       ("feature_info", feature_filename, bigquery.SourceFormat.AVRO),
+        ("raw_count_matrix", raw_counts_pattern, bigquery.SourceFormat.CSV),
     ]
     job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.AVRO)
 
-    for table, file in pairs:
+    for table, file_pattern, format in pairs:
         table = f"cas_{table}"
         table_id = f"{project}.{dataset}.{table}"
 
-        stage_file(file)
-        staged_files.append(file)
+        for file in glob.glob(file_pattern):
+            stage_file(file)
+            staged_files.append(file)
 
-        uri = f"{gcs_path_prefix}/{file}"
+        uri = f"{gcs_path_prefix}/{file_pattern}"
+        job_config = bigquery.LoadJobConfig(source_format=format)
+
         load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)  # Make an API request.
         result = load_job.result()  # Waits for the job to complete.
         print(f"{result.output_rows} rows loaded into BigQuery table '{table_id}'.")
