f6cc1a0c718921349fdfb0bb03afcb2475831af6
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-01-05 23:49:30 +0000

    read_h5ad_blob

---
+++ src/casp/anndata/read.py
@@ -0,0 +1,29 @@
+import re
+
+import anndata
+from google.cloud import storage
+
+
+def read_h5ad_blob(
+    filename: str,
+) -> anndata.AnnData:
+    """
+    Read `.h5ad`-formatted hdf5 file from the Google Cloud Storage blob.
+
+    Parameters
+    ----------
+    filename
+        File path to the data file in Cloud Storage.
+    """
+    if filename.startswith("gs:"):
+        filename = re.sub(r"^gs://?", "", filename)
+
+    # parse bucket and blob names from the filename
+    bucket_name, blob_name = filename.split("/", 1)
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    with blob.open("rb") as f:
+        return anndata.read_h5ad(f)
