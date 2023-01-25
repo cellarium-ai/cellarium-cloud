759d99c58fe87f9bb307b7ee10fac3ff108116fa
Fedor Grab <fvgrab@gmail.com>
2023-01-25 12:42:08 -0500

    WIP

--- src/casp/services/admin/__init__.py
+++ src/casp/services/admin/__init__.py
@@ -12,12 +12,9 @@ db_session = db.init_db()
 @basic_auth.verify_password
 def verify_password(username, password):
     if username == settings.ADMIN_BASIC_AUTH_USER.get("username") and password == settings.ADMIN_BASIC_AUTH_USER.get(
-        "password"
+            "password"
     ):
         return username
 
-
-with flask_app.app_context():
-    pass
 
 import casp.services.admin.views  # noqa
