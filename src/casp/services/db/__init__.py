ce5d67a7835d89be06f4cfb3caed5edd695c1c49
fedorgrab <fvgrab@gmail.com>
2023-11-29 14:28:33 -0500

    Fix: #109 Increase database connection pool size (#111)
    * Fix: #109 Increase database connection pool size
    
    * Fix: More settings to Cloud Run Deployment docs

--- src/casp/services/db/__init__.py
+++ src/casp/services/db/__init__.py
@@ -5,10 +5,10 @@ from casp.services import settings
 
 engine = sqlalchemy.create_engine(
     url=settings.SQLALCHEMY_DATABASE_URI,
-    pool_size=5,
-    max_overflow=2,
-    pool_timeout=30,  # 30 seconds
-    pool_recycle=1800,  # 30 minutes
+    pool_size=settings.DB_CONNECTION_POOL_SIZE,
+    max_overflow=settings.DB_CONNECTION_POOL_MAX_OVERFLOW,
+    pool_timeout=settings.DB_CONNECTION_POOL_TIMEOUT,
+    pool_recycle=settings.DB_CONNECTION_POOL_RECYCLE,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db_session = scoped_session(SessionLocal)
