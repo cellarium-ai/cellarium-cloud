b3cde46b3921038838cb917e47ef8eaf0b158ecc
Fedor Grab <fvgrab@gmail.com>
2024-03-22 11:17:30 -0400

    WIP:

--- src/casp/services/api/services/consensus_engine.py
+++ src/casp/services/api/services/consensus_engine.py
@@ -1,13 +1,20 @@
+import pickle
 import typing as t
+
 import numpy as np
-import scipy.sparse as sp
 from google.cloud.aiplatform.matching_engine.matching_engine_index_endpoint import MatchNeighbor
-from casp.services.api.data_manager import CellOperationsDataManager
-from casp.services.api import schemas
-from casp.services import settings
 from smart_open import open
 
+from casp.services import settings
+from casp.services.api import schemas
+from casp.services.api.data_manager import CellOperationsDataManager
+
 DEFAULT_SUMMARIZE_NEIGHBOR_CONTEXT_METHOD = "ontology_aware"
+
+
+# class CellOntologyResource:
+#     def __init__(self, cell_ontology_ancestors_dict: t.Dict[str, t.List[str]]):
+#         self.cell_ontology_ancestors_dict = cell_ontology_ancestors_dict
 
 
 class ConsensusEngine:
@@ -15,66 +22,100 @@ class ConsensusEngine:
     def __init__(self):
         self.cell_operations_dm = CellOperationsDataManager()
 
-        with open(settings.GCS_CELL_ONTOLOGY_SPARSE_MATRIX_NPZ, "rb") as f:
-            # self.cell_ontology_csr_matrix = sp.load_npz(f)
-        self.cell_ontology_csr_matrix = sp.load_npz("cell_ontology_sparse_matrix.npz")
+        # with open(settings.GCS_CELL_ONTOLOGY_SPARSE_MATRIX_NPZ, "rb") as f:
+        with open("ancestors_dictionary.pickle", "rb") as f:
+            self.cell_type_ontology_ancestors_dictionary = pickle.load(f.read())
 
-    # def summarize_neighbor_context_ontology_aware_method(
-    #     self, query_ids: t.List[str], query_neighbors: t.List[t.List[MatchNeighbor]]
-    # ) -> t.List:
-    #     unique_neighbor_ids = set()
-    #
-    #     for neighbors in query_neighbors:
-    #         for neighbor in neighbors:
-    #             unique_neighbor_ids.add(neighbor.id)
+    def get_ontology_aware_neighborhood_context(
+        self,
+        neighbors: t.List[MatchNeighbor],
+        neighbors_metadata_dict: t.Dict[str, schemas.CellariumCellMetadata],
+        neighbors_cell_type_id_to_name_dict: t.Dict[str, str],
+        normalize: bool = True,
+    ) -> t.List[t.Dict[str, t.Any]]:
+        """
+        Get the neighborhood context for a query cell using the ontology-aware method. The ontology-aware method assigns
+        weights to each neighbor based on their distance and the cell type ontology.
 
-    def ontology_aware_summarize_neighbor_context(
-        self, neighbors: t.List[MatchNeighbor], neighbor_metadata_dict: t.Dict[str, schemas.CellariumCellMetadata]
-    ) -> t.Dict[str, t.Any]:
+        :param neighbors: Neighbors of the query cell from the matching engine
+        :param neighbors_metadata_dict: Dictionary of neighbor metadata
+        :param neighbors_cell_type_id_to_name_dict: Dictionary of cell type ontology term IDs to cell type names
+        :param normalize: Whether to normalize the scores by the total weight
+
+        :return: Dictionary of scores for each cell type ontology term
+        """
         neighbor_distances = np.asarray([neighbor.distance for neighbor in neighbors])
-        neighbor_metadata = [neighbor_metadata_dict[neighbor.id] for neighbor in neighbors]
-        gamma = 1. / np.median(neighbor_distances)
+        neighbor_metadata = [neighbors_metadata_dict[neighbor.id] for neighbor in neighbors]
+
+        # Get weights for each neighbor
+        gamma = 1.0 / np.median(neighbor_distances)
         weights = np.exp(gamma * neighbor_distances)
 
         total_weight = 0
         total_usable_neighbors = 0
-        # scores_array =
+        scores_dict = {k: 0 for k in self.cell_type_ontology_ancestors_dictionary.keys()}
 
-        for weight, neighbor_metadata in zip(weights, neighbor_metadata):
-            neighbor_metadata["weight"] = weight
+        for neighbor_metadata, weight in zip(neighbor_metadata, weights):
+            # Cell Ontology IDs have the formats: CL:0000000 (in our database) and CL_0000000 (in the ontology graph)
+            neighbor_cell_type_ontology_id = neighbor_metadata.cell_type_ontology_term_id.replace(":", "_")
 
-            neighbor_cl_name = neighbor_metadata.cell_type_ontology_term_id.replace(':', '_')
+            # if neighbor_cell_type_ontology_term_id not in allowed_cl_names_set:
+            #     continue
 
-            if neighbor_cl_name not in allowed_cl_names_set:
-                continue
-
-            nn_cl_idx = cl_names_to_idx_map[nn_cl_name]
-            total_usable_neighbors += 1
             total_weight += weight
-            scores_array[nn_cl_idx] += weight
-            # propagate the weight to all ancestors (consistent subgraph of the CL onotlogy)
-            scores_array[cl_ancestors_csr_matrix[nn_cl_idx].indices] += weight
+            total_usable_neighbors += 1
 
+            # Add weight to the neighbor's ontology ID
+            scores_dict[neighbor_cell_type_ontology_id] += weight
 
+            # Propagate the weight to all ancestors (consistent subgraph of the cell type ontology)
+            ancestor_ontology_ids = self.cell_type_ontology_ancestors_dictionary[neighbor_cell_type_ontology_id]
+
+            for ancestor in ancestor_ontology_ids:
+                scores_dict[ancestor] += weight
+
+        if normalize:
+            scores_dict = {k: v / total_weight for k, v in scores_dict.items()}
+
+        result = [
+            {"cell_type_ontology_term_id": k, "cell_type": neighbors_cell_type_id_to_name_dict[k], "score": v}
+            for k, v in scores_dict.items()
+        ]
+
+        return result
 
     def summarize_query_neighbor_context(
         self,
         query_ids: t.List[str],
         knn_query: t.List[t.List[MatchNeighbor]],
-        method: str = DEFAULT_SUMMARIZE_NEIGHBOR_CONTEXT_METHOD
+        method: str = DEFAULT_SUMMARIZE_NEIGHBOR_CONTEXT_METHOD,
+        normalize: bool = True,
     ) -> t.List:
         unique_neighbor_ids = set(int(neighbor.id) for query_neighbors in knn_query for neighbor in query_neighbors)
-        neighbor_metadata = self.cell_operations_dm.get_cell_metadata_by_ids(list(unique_neighbor_ids))
-        neighbor_metadata_dict = {neighbor.cas_cell_index: neighbor for neighbor in neighbor_metadata}
+        neighbors_metadata = self.cell_operations_dm.get_cell_metadata_by_ids(list(unique_neighbor_ids))
+        neighbors_metadata_dict = {neighbor.cas_cell_index: neighbor for neighbor in neighbors_metadata}
+        neighbors_cell_type_id_to_name_dict = {
+            neighbor.cell_type_ontology_term_id: neighbor.cell_type for neighbor in neighbors_metadata
+        }
 
         result = []
         for query_id, query_neighbors in zip(query_ids, knn_query):
             match method:
                 case "ontology_aware":
-                    neighbor_context = self.ontology_aware_summarize_neighbor_context(query_neighbors)
+                    neighbor_context = self.get_ontology_aware_neighborhood_context(
+                        neighbors=query_neighbors,
+                        neighbors_metadata_dict=neighbors_metadata_dict,
+                        neighbors_cell_type_id_to_name_dict=neighbors_cell_type_id_to_name_dict,
+                        normalize=normalize,
+                    )
                 case _:
                     raise ValueError(f"Unknown method: {method}")
 
             result.append({"query_cell_id": query_id, "matches": neighbor_context})
 
         return result
+
+    async def summarize_query_neighbor_context_async(
+            self, query_ids: t.List[str], knn_query: t.List[t.List[MatchNeighbor]], method: str = DEFAULT_SUMMARIZE_NEIGHBOR_CONTEXT_METHOD, normalize: bool = True
+    ) -> t.List:
+        return self.summarize_query_neighbor_context(query_ids, knn_query, method, normalize)
