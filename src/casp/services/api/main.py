8aeb3fbba62ad4b3e2ebe19b5c3336fca28c619c
nmalfroy <nmalfroy@broadinstitute.org>
2024-04-16 13:22:03 -0400

    Add wrapper around services
    So that we have a common way of defining service behavior

--- src/casp/services/api/main.py
+++ src/casp/services/api/main.py
@@ -1,38 +1,18 @@
-import multiprocessing
-
-import sentry_sdk
-import uvicorn
-from fastapi import FastAPI
-
 from casp.services import settings
-from casp.services.api import exception_handlers
 from casp.services.api.routers import cell_operations_router, cellarium_general_router
-from casp.services.api.services import exceptions
+from casp.services.app import CASService, RouterDef
 
-sentry_sdk.init(
-    dsn=settings.SENTRY_DSN,
-    server_name="api-service",
-    enable_tracing=settings.SENTRY_ENABLE_TRACING,
-    profiles_sample_rate=settings.SENTRY_PROFILES_SAMPLE_RATE,
-    traces_sample_rate=settings.SENTRY_TRACES_SAMPLE_RATE,
-)
-
-application = FastAPI(
+application = CASService(
     title="Cellarium Cloud API",
     description="Cellarium Cloud Application API",
-    version=settings.APP_VERSION,
-    docs_url="/api/docs",
-    redoc_url="/api/redoc",
+    plugins=None,
+    routers=[
+        RouterDef(router=cellarium_general_router, tags=["cellarium-general"]),
+        RouterDef(router=cell_operations_router, tags=["cell-operations"]),
+    ],
+    sentry_application_id="api-service",
+    local_port=settings.API_SERVICE_PORT
 )
-API_SERVICE_PREFIX = "/api"
-
-application.include_router(router=cellarium_general_router, prefix=API_SERVICE_PREFIX, tags=["cellarium-general"])
-application.include_router(router=cell_operations_router, prefix=API_SERVICE_PREFIX, tags=["cell-operations"])
-
-application.add_exception_handler(exceptions.AccessDeniedError, exception_handlers.access_denied_error_handler)
-application.add_exception_handler(exceptions.InvalidInputError, exception_handlers.invalid_input_error_handler)
 
 if __name__ == "__main__":
-    num_workers = 2 if settings.ENVIRONMENT == "local" else multiprocessing.cpu_count() * 2 + 1
-    port = settings.API_SERVICE_PORT if settings.ENVIRONMENT == "local" else settings.DEFAULT_SERVICE_PORT
-    uvicorn.run("main:application", host=settings.DEFAULT_SERVICE_HOST, port=port, workers=num_workers)
+    application.run()
---
+++ src/casp/services/app.py
@@ -0,0 +1,143 @@
+import typing as t
+import multiprocessing
+import time
+
+import sentry_sdk
+from fastapi import FastAPI, APIRouter, Request, Response
+from starlette.middleware import Middleware
+from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction
+from starlette_context import context, plugins, request_cycle_context
+from starlette_context.plugins import Plugin
+from starlette_context.middleware import RawContextMiddleware
+import uvicorn
+import uvicorn.config
+import google.cloud.logging
+
+from casp.services import settings
+from casp.services.api import exception_handlers
+from casp.services.api.services import exceptions
+
+
+class RouterDef(t.NamedTuple):
+    router: APIRouter
+    prefix: str = "/api"
+    tags: t.List[str] = []
+
+
+class ExceptionHandlerDef(t.NamedTuple):
+    exception: Exception
+    handler: t.Callable
+
+
+BASE_PLUGGINS: t.Sequence[Plugin] = (
+    # Adds an x-request-id header to all responses and makes it available to the request context.
+    plugins.RequestIdPlugin(),
+    # Adds an x-correlation-id header to all responses and makes it available to the request context.
+    plugins.CorrelationIdPlugin(),
+    # Extracts the user-agent header and makes it available to the request context.
+    plugins.UserAgentPlugin(),
+)
+
+
+BASE_ERROR_HANDLERS: t.List[ExceptionHandlerDef] = [
+    ExceptionHandlerDef(exception=exceptions.AccessDeniedError, handler=exception_handlers.access_denied_error_handler),
+    ExceptionHandlerDef(exception=exceptions.InvalidInputError, handler=exception_handlers.invalid_input_error_handler),
+]
+
+
+class TimingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: DispatchFunction) -> Response:
+        start_time = time.time()
+        response = await call_next(request)
+        request_time = time.time() - start_time
+        context["request_time"] = request_time
+        return response
+
+
+class CASService(FastAPI):
+    """
+    Wrapper around FastAPI that provides some common configuration for CAS services.
+    """
+
+    def __init__(
+            self,
+            local_port: int,
+            plugins: t.Optional[t.Sequence[Plugin]] = None,
+            routers: t.List[RouterDef] = None,
+            exception_handlers: t.Optional[t.List[ExceptionHandlerDef]] = None,
+            sentry_application_id: str = "",
+            *args,
+            **kwargs,
+    ):
+        """
+        Initialize the service with the given configuration.
+
+        @param local_port: The port to use when running the service locally.
+        @param plugins: A list of middleware plugins to use for the service.
+        @param routers: A list of routers to include in the service.
+        @param exception_handlers: A list of exception handlers to include in the service in addition to the default ones
+                                   as defined in BASE_ERROR_HANDLERS
+        @param sentry_application_id: The application ID to use when reporting errors to Sentry.    
+        """
+
+        self.local_port = local_port
+
+        # Configure Sentry integration
+        sentry_sdk.init(
+            dsn=settings.SENTRY_DSN,
+            server_name=sentry_application_id,
+            enable_tracing=settings.SENTRY_ENABLE_TRACING,
+            profiles_sample_rate=settings.SENTRY_PROFILES_SAMPLE_RATE,
+            traces_sample_rate=settings.SENTRY_TRACES_SAMPLE_RATE,
+        )
+
+        # Init stackdriver logging
+        client = google.cloud.logging.Client()
+        client.setup_logging()
+
+        # Configure middleware
+        if (plugins):
+            _plugins = BASE_PLUGGINS + plugins
+        else:
+            _plugins = BASE_PLUGGINS
+        middleware = [
+            Middleware(
+                RawContextMiddleware,
+                plugins=_plugins
+            ),
+            Middleware(
+                TimingMiddleware
+            )
+        ]
+
+        # Perform basic initialization
+        super().__init__(
+            *args,
+            **kwargs,
+            version=settings.APP_VERSION,
+            docs_url="/api/docs",
+            redoc_url="/api/redoc",
+            swagger_ui_parameters={"displayRequestDuration": True},
+            middleware=middleware)
+
+        # Register routers
+        for router_def in routers:
+            self.include_router(router=router_def.router, prefix=router_def.prefix, tags=router_def.tags)
+
+        # Register exception handlers
+        for handler_def in exception_handlers or []:
+            self.add_exception_handler(handler_def.exception, handler_def.handler)
+
+    def run(self):
+        """
+        Launch the service using Uvicorn.
+        """
+        num_workers = 2 if settings.ENVIRONMENT == "local" else multiprocessing.cpu_count() * 2 + 1
+        port = self.local_port if settings.ENVIRONMENT == "local" else settings.DEFAULT_SERVICE_PORT
+
+        uvicorn.run("main:application",
+                    host=settings.DEFAULT_SERVICE_HOST,
+                    port=port,
+                    workers=num_workers,
+                    log_level=settings.LOG_LEVEL,
+                    log_config=settings.LOG_CONFIG)
