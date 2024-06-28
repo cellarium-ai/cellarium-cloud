dc27b625e0d1daeb85fdbaacb6c5efe2982c0e43
Fedor Grab <fvgrab@gmail.com>
2024-06-28 18:41:06 +0200

    Update resize component

--- src/casp/workflows/kubeflow/job_components/pca.py
+++ src/casp/workflows/kubeflow/job_components/pca.py
@@ -72,6 +72,10 @@ def resize_and_save(gcs_config_path: str) -> None:
     # Feels like it was updated in newer version of Cellarium ML
     # model_ckpt["hyper_parameters"]["model"]["init_args"]["n_components"] = new_embedding_dimension
     model_ckpt["hyper_parameters"]["model"].n_components = new_embedding_dimension
+    model_ckpt["hyper_parameters"]["model"].V_kg = model_ckpt["hyper_parameters"]["model"].V_kg[
+        :new_embedding_dimension
+    ]
+    model_ckpt["hyper_parameters"]["model"].S_k = model_ckpt["hyper_parameters"]["model"].S_k[:new_embedding_dimension]
 
     with open(config_data["new_model_path"], "wb") as file:
         torch.save(model_ckpt, file)
