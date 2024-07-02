f0374a62e23fc401a9c5d2360dc77e928273d0e0
Kevin Lydon <klydon@broadinstitute.org>
2024-07-02 11:55:11 -0400

    Now, instead of allowing None, description sent to the user will default to empty string

--- src/casp/services/api/schemas/cellarium_general.py
+++ src/casp/services/api/schemas/cellarium_general.py
@@ -1,15 +1,19 @@
 import datetime
-from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class CASModel(BaseModel):
     model_name: str = Field(example="human-pca-001")
-    description: Optional[str] = Field(example="1st version of PCA model for human data")
+    description: str = Field(example="1st version of PCA model for human data", default="")
     schema_name: str = Field(example="refdata-gex-mm10-2020-A")
     is_default_model: bool = Field(example=False)
     embedding_dimension: int = Field(example=512)
+
+    # Set the default value for description if it is provided as None
+    @validator('description', pre=True, always=True)
+    def set_description_default(cls, v):
+        return v or ""
 
     class Config:
         orm_mode = True
