2fc10a41afbdd0f54066344d99b9deee37b3812d
Fedor Grab <fvgrab@gmail.com>
2024-07-09 12:41:14 +0200

    Add retrying to generate cas outputs

--- src/casp/scripts/benchmarking/generate_cas_outputs.py
+++ src/casp/scripts/benchmarking/generate_cas_outputs.py
@@ -1,11 +1,16 @@
 import pickle
 import typing as t
+import time
 
 import anndata
-from cellarium.cas import CASClient
+from cellarium.cas import CASClient, exceptions as cas_exceptions
 from smart_open import open
 
 from casp.scripts.benchmarking import utils
+
+
+class AnnotationError(Exception):
+    pass
 
 
 def generate_cas_outputs(
@@ -26,19 +31,39 @@ def generate_cas_outputs(
     :param cas_results_output_path: Path to save the CAS results.
     """
     _dataset_file_paths = utils.get_paths(paths=dataset_paths)
+    delay = 30
+    num_attempts = 5
+
     for dataset_file_path in _dataset_file_paths:
-        print(f"Running CAS over {dataset_file_path} dataset")
+        print(f"Running CAS over {dataset_file_path} dataset...")
 
         cas_client = CASClient(api_token=cas_api_token, api_url=cas_api_url)
 
         with open(dataset_file_path, "rb") as f:
             adata = anndata.read_h5ad(f)
 
-        cas_result = cas_client.annotate_matrix_cell_type_ontology_aware_strategy(
-            matrix=adata,
-            cas_model_name=model_name,
-            chunk_size=1000,
-        )
+        curr_attempt = 0
+        done = False
+        while curr_attempt <= num_attempts and not done:
+            try:
+                cas_result = cas_client.annotate_matrix_cell_type_ontology_aware_strategy(
+                    matrix=adata,
+                    cas_model_name=model_name,
+                    chunk_size=1000,
+                )
+            except cas_exceptions.DataProcessingError:
+                time.sleep(delay)
+                curr_attempt += 1
+                if curr_attempt < num_attempts:
+                    print(f"Retrying annotation, number of current retry: {curr_attempt}...")
+                    continue
+
+                raise AnnotationError("Exceeded number of allowed retries. Exiting.")
+            else:
+                done = True
+                print("Successfully finished annotation.")
+
+        print("Uploading output to bucket...")
         dataset_file_name = dataset_file_path.split("/")[-1].split(".")[0]
         output_path = f"{cas_results_output_path}/{model_name}/cas_output_{dataset_file_name}.pickle"
 
