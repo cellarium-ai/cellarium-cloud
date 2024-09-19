af58753376a6b4ba3b6c461a7adb5125538a31c7
Kevin Lydon <klydon@broadinstitute.org>
2024-09-19 15:29:52 -0400

    Forgot a file

---
+++ src/casp/services/db/migrations/versions/t_2024-09-18_12-04-55_lifetime_quotas.py
@@ -0,0 +1,24 @@
+"""lifetime_quotas
+
+Revision ID: c80bd73203d1
+Revises: 2e2ff7bcff8a
+Create Date: 2024-09-18 12:04:55.133331
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c80bd73203d1'
+down_revision = '2e2ff7bcff8a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('users_user', sa.Column('lifetime_cell_quota', sa.Integer(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users_user', 'lifetime_cell_quota')
