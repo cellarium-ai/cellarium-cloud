0c24fa362cdc4b1203e851856a12e533aa34943c
Fedor Grab <fvgrab@gmail.com>
2024-10-01 18:05:31 +0200

    Fix large batch sizes in extract

--- src/casp/scripts/bq_ops/extract_metadata_utils.py
+++ src/casp/scripts/bq_ops/extract_metadata_utils.py
@@ -4,6 +4,9 @@ from google.cloud import bigquery
 from casp.scripts.bq_ops import constants
 
 
+COLUMNS_TO_IGNORE = ["original_feature_id"]
+
+
 def get_measured_genes_info_filepath(bucket_name: str, extract_bucket_path: str) -> str:
     return (
         f"gs://{bucket_name}/{extract_bucket_path}"
@@ -27,5 +30,6 @@ def get_categorical_column_names_from_cell_info_table(
 
     table_ref = bigquery.TableReference.from_string(f"{project}.{dataset}.{extract_table_prefix}__extract_cell_info")
     table = client.get_table(table_ref)
-
-    return [schema_field.name for schema_field in table.schema if schema_field.field_type == "STRING"]
+    _column_names = [schema_field.name for schema_field in table.schema if schema_field.field_type == "STRING"]
+    column_names = [column_name for column_name in _column_names if column_name not in COLUMNS_TO_IGNORE]
+    return column_names
