2cabc3811db43774848366da3c6a23650df8e283
Kevin Lydon <klydon@broadinstitute.org>
2024-10-24 13:32:30 -0400

    Forgot the migration file

---
+++ src/casp/services/db/migrations/versions/t_2024-10-23_13-39-07_quota_increase.py
@@ -0,0 +1,25 @@
+"""quota_increase
+
+Revision ID: cdfe110d1068
+Revises: c80bd73203d1
+Create Date: 2024-10-23 13:39:07.609718
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cdfe110d1068'
+down_revision = 'c80bd73203d1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('users_user', sa.Column('quota_increased', sa.Boolean(), nullable=False, server_default='False'))
+    op.execute("update users_user set quota_increased = true where lifetime_cell_quota = 200000")
+
+
+def downgrade() -> None:
+    op.drop_column('users_user', 'quota_increased')
