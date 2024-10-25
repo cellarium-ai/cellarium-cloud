bf413dc20ba18260657cb00ea2f7dfe46e21f2f7
Kevin Lydon <klydon@broadinstitute.org>
2024-10-25 11:36:17 -0400

    When creating a new user, admin defaults to false now

--- src/casp/services/db/models/users.py
+++ src/casp/services/db/models/users.py
@@ -18,7 +18,7 @@ class User(db.Base):
     active = sa.Column(sa.Boolean(), default=True, nullable=False)
     requests_processed = sa.Column(sa.Integer, default=0, nullable=False)
     cells_processed = sa.Column(sa.Integer, default=0, nullable=False)
-    is_admin = sa.Column(sa.Boolean(), default=True, nullable=False)
+    is_admin = sa.Column(sa.Boolean(), default=False, nullable=False)
     cell_quota = sa.Column(sa.Integer(), default=100000, nullable=False)
     lifetime_cell_quota = sa.Column(sa.Integer(), default=settings.DEFAULT_LIFETIME_QUOTA, nullable=True)
     quota_increased = sa.Column(sa.Boolean(), default=False, nullable=False)
