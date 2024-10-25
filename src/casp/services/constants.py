786b2307a1055c8b4e6361fafe36eae0b2d807b5
Kevin Lydon <KevinCLydon@gmail.com>
2024-10-25 12:01:57 -0400

    Updated app version and minimum client version for release (#193)

--- src/casp/services/constants.py
+++ src/casp/services/constants.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 # The minimum client version that is allowed to connect to the server.
-MIN_CLIENT_VERSION: str = "1.6.0"
+MIN_CLIENT_VERSION: str = "1.7.0"
 
 
 class ContextKeys(str, Enum):
