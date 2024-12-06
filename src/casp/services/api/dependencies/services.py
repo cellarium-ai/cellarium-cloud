55a394b80837f21fc6ee179ff8a3c586b5117210
fedorgrab <fvgrab@gmail.com>
2024-12-06 14:35:47 -0300

    Feat: Implement Summary stats strategy using postgres

--- src/casp/services/api/dependencies/services.py
+++ src/casp/services/api/dependencies/services.py
@@ -7,3 +7,7 @@ def get_cell_operations_service() -> services.CellOperationsService:
 
 def get_cellarium_general_service() -> services.CellariumGeneralService:
     return services.CellariumGeneralService()
+
+
+def get_cell_quota_service() -> services.CellQuotaService:
+    return services.CellQuotaService()
