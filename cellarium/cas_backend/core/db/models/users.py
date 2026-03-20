5a869197abff4434ff406f62f7c6cc4288975a59
fedorgrab <fvgrab@gmail.com>
2026-03-20 13:15:35 -0400

    refactor: move user key expiration logic to a function

--- cellarium/cas_backend/core/db/models/users.py
+++ cellarium/cas_backend/core/db/models/users.py
@@ -11,6 +11,10 @@ from cellarium.cas_backend.core.db.field_type_decorators import SQLiteCompatible
 CURRENT_TOKEN_VERSION: str = "2"
 
 
+def _default_user_key_expiration() -> datetime.datetime:
+    return datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=settings.JWT_DEFAULT_TOKEN_TTL)
+
+
 class User(db.Base):
     id = sa.Column(sa.Integer, primary_key=True)
     email = sa.Column(sa.String(255), unique=True, nullable=False)
@@ -79,7 +83,7 @@ class UserKey(db.Base):
     active = sa.Column(sa.Boolean(), default=True, nullable=False)
     expires = sa.Column(
         sa.DateTime,
-        default=datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=settings.JWT_DEFAULT_TOKEN_TTL),
+        default=_default_user_key_expiration,
         nullable=False,
     )
     # A hash of the user's key.  Adding deferred to prevent loading the key unless it is needed to avoid leaking it
